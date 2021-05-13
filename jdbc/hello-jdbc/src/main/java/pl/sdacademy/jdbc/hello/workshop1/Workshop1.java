7c4cd043130fedb59aa5f666749b9779d4977416
Kinga Izdebska <kinga.izdebska@gmail.com>
2021-05-13 19:52:42 +0200

    workshop1

--- jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop1/Workshop1.java
+++ jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop1/Workshop1.java
@@ -1,6 +1,9 @@
 package pl.sdacademy.jdbc.hello.workshop1;
 
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Workshop1 {
     public static void main(String[] args) {
@@ -10,6 +13,18 @@ public class Workshop1 {
     }
 
     private static List<String> getCountries() {
-        throw new UnsupportedOperationException("TODO");
+        //throw new UnsupportedOperationException("TODO");
+        List<String> result = new LinkedList<>();
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "Pomidor1#")) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("select name from country where continent = 'Europe' order by name;")) {
+                ResultSet rs = preparedStatement.executeQuery();
+                while ( rs.next() ) {
+                    result.add(rs.getString("Name"));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
     }
 }
